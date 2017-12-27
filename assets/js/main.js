57dc4203feb614b74bbc604edae00146eac68f5f
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2017-12-27 18:42:30 -0500

    Implemented a fully functional navigation bar.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
 (function($) {
 
 	var settings = {
