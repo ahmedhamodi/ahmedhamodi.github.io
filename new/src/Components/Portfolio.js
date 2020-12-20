b67acdbdc6f333f7020c0a8ea737cb95f92d2aac
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2020-12-20 13:03:36 -0500

    Finished education section and updated some CSS.

--- new/src/Components/Portfolio.js
+++ new/src/Components/Portfolio.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Portfolio extends Component {
   render() {
-
     if (this.props.data) {
       var projects = this.props.data.projects.map(function (projects) {
-        var projectImage = 'images/portfolio/' + projects.image;
-        
+        var projectImage = "images/portfolio/" + projects.image;
+
         return (
           <div key={projects.title} className="columns portfolio-item">
             <div className="item-wrap">
@@ -21,20 +20,20 @@ class Portfolio extends Component {
               </a>
             </div>
           </div>
-        )
-      })
+        );
+      });
     }
 
     return (
       <section id="portfolio">
-
         <div className="row">
-
           <div className="twelve columns collapsed">
-
             <h1>Check Out Some of My Works.</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
               {projects}
             </div>
           </div>
--- new/src/Components/Resume.js
+++ new/src/Components/Resume.js
@@ -1,92 +1,127 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Resume extends Component {
-
   getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
+    var letters = "0123456789ABCDEF";
+    var color = "#";
     for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
   }
-  
+
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
-      })
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+      var education = this.props.data.education.map(function (education) {
+        return (
+          <div key={education.school}>
+            <h3>{education.school}</h3>
+            <p className="info">
+              {education.degree} <span>&bull;</span>
+              <em className="date">{education.graduated}</em>
+            </p>
+            <p>{education.description}</p>
+          </div>
+        );
+      });
+      var work = this.props.data.work.map(function (work) {
+        return (
+          <div key={work.company}>
+            <h3>{work.company}</h3>
+            <p className="info">
+              {work.title}
+              <span>&bull;</span> <em className="date">{work.years}</em>
+            </p>
             <p>{work.description}</p>
-        </div>
-      })
+          </div>
+        );
+      });
 
-      var skills = this.props.data.skills.map((skills)=>{
-        var className = 'bar-expand '+skills.name.toLowerCase();
+      var skills = this.props.data.skills.map((skills) => {
+        var className = "bar-expand " + skills.name.toLowerCase();
         return (
           <li key={skills.name}>
-            <span style={{width:skills.level, backgroundColor:this.getRandomColor()}}className={className}></span><em>{skills.name}</em>
+            <span
+              style={{
+                width: skills.level,
+                backgroundColor: this.getRandomColor(),
+              }}
+              className={className}
+            ></span>
+            <em>{skills.name}</em>
           </li>
-        )
-      })
+        );
+      });
     }
 
     return (
       <section id="resume">
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
 
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
+          <div className="nine columns main-col">
+            <p>{skillmessage}</p>
 
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
-            </div>
-         </div>
-      </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
+            <table style={{ width: "100%", borderWidth: "thick" }}>
+              <tr>
+                <th style={{ textAlign: "left", minWidth: "120px" }}>
+                  <b>Languages</b>
+                </th>
+                <td colspan="3">
+                  Python, Javascript, PHP, C++, C#, C, Java, PostgreSQL, MySQL,
+                  Bash
+                </td>
+              </tr>
+              <tr>
+                <th style={{ textAlign: "left", minWidth: "120px" }}>
+                  <b>Frameworks</b>
+                </th>
+                <td colspan="3">
+                  React, Node.js, AWS, Laravel, .NET Core, TensorFlow, Keras
+                </td>
+              </tr>
+              <tr>
+                <th style={{ textAlign: "left", minWidth: "120px" }}>
+                  <b>Utilities</b>
+                </th>
+                <td colspan="3">
+                  Git, Firebase, Docker, Heroku, Jenkins, Arduino, Google
+                  Analytics, JIRA
+                </td>
+              </tr>
+            </table>
+          </div>
         </div>
-    </div>
 
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Work</span>
+            </h1>
+          </div>
 
+          <div className="nine columns main-col">{work}</div>
+        </div>
 
-      <div className="row skill">
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
 
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
-   </section>
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">{education}</div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
