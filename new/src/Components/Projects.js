e092fde940edb562f73c07df7deb1f3f019d3417
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2020-12-20 14:34:39 -0500

    Populate carousal with projects.

--- new/src/Components/Projects.js
+++ new/src/Components/Projects.js
@@ -1,33 +1,52 @@
 const Projects = [
   {
-    title: "Project #1",
-    category: "Info about project #1",
-    image: require("../Images/portfolio/thumbs/coding.jpg"),
-    url: "https://github.com/ahmedhamodi",
+    title: "Meetability",
+    category:
+      "Integrated Google Maps API, Firebase, and a React (with Redux store) frontend to create the web application. Created sign up and login work flows for families and individuals with disabilities to meet. Created an internal messaging platform within the app, allowing users to send real time messages to each other.",
+    image: require("../Images/portfolio/thumbs/meetability.png"),
+    url: "https://github.com/ahmedhamodi/meetability",
   },
   {
-    title: "Project #2",
-    category: "Info about project #2",
-    image: require("../Images/portfolio/thumbs/ecar.jpg"),
-    url: "https://github.com/ahmedhamodi",
+    title: "Smile4Real",
+    category:
+      "Developed an Android application that provides activities and quotes, through a paginated scroll view, which are tailored to help encourage mental well-being, placing 2nd place at DeltaHacks. Implemented features such as an infinite image/text scroll, an external call link to the suicide helpline number, and API requests.",
+    image: require("../Images/portfolio/thumbs/smile4real.png"),
+    url: "https://github.com/ahmedhamodi/smile4real",
   },
   {
-    title: "Project #3",
-    category: "Info about project #3",
-    image: require("../Images/portfolio/thumbs/arbitrium.png"),
-    url: "https://github.com/ahmedhamodi",
-  },
-  {
-    title: "Project #4",
-    category: "Info about project #4",
-    image: require("../Images/portfolio/thumbs/maxaspire.png"),
-    url: "https://github.com/ahmedhamodi",
-  },
-  {
-    title: "Project #5",
-    category: "Info about project #5",
+    title: "OneMAX",
+    category:
+      "Developed search and filter functionality on the nominee page, interacting with API endpoints. Improved page load time by 40% through implementing proper pagination of the nominee page. Designed the REST API model, developed with Ruby and Go, which is deployed on Heroku.",
     image: require("../Images/portfolio/thumbs/onemax.png"),
-    url: "https://github.com/ahmedhamodi",
+    url: "https://github.com/ahmedhamodi/onemax",
+  },
+  {
+    title: "Mood Detection Box",
+    category:
+      "Used Twitter API to parse trending categories of tweets, then used IBM Watson API for sentiment analysis to determine the strength of certain emotions displayed through the tweets. Integrated an LED display on an Arduino which displays the emotion through a pulsating effect.",
+    image: require("../Images/portfolio/thumbs/mood-box.png"),
+    url: "https://github.com/ahmedhamodi/twitter-mood-detection-box",
+  },
+  {
+    title: "Defense Matrix",
+    category:
+      "Created a server-side security suite in Python for Linux servers which automates firewall configuration, does intrusion detection, and enforces a more secure password criterion.",
+    image: require("../Images/portfolio/thumbs/defensematrix.jpg"),
+    url: "https://github.com/ahmedhamodi/defense_matrix_security",
+  },
+  {
+    title: "Synk Programming",
+    category:
+      "Programmed a Python application that allows multiple users to simultaneously program on a single script, winning third place at JAMHacks.",
+    image: require("../Images/portfolio/thumbs/jamhacks-team.jpg"),
+    url: "https://github.com/ahmedhamodi/synk_programming",
+  },
+  {
+    title: "Personal Website",
+    category:
+      "Created a personal portfolio website using React to display a variety of my accomplishments visually.",
+    image: require("../Images/portfolio/thumbs/coding.jpg"),
+    url: "https://github.com/ahmedhamodi/ahmedhamodi.github.io",
   },
 ];
 
--- new/src/Components/SpotlightProjects.js
+++ new/src/Components/SpotlightProjects.js
@@ -3,19 +3,19 @@ const Projects = [
     title: "Arbitrium",
     category: "Info about project #1",
     image: require("../Images/portfolio/thumbs/arbitrium.png"),
-    url: "https://github.com/ahmedhamodi",
+    url: "https://github.com/uwblueprint/cookie-monster",
   },
   {
     title: "MAX Aspire",
     category: "Info about project #2",
     image: require("../Images/portfolio/thumbs/maxaspire.png"),
-    url: "https://github.com/ahmedhamodi",
+    url: "https://github.com/maxgala/aspire-frontend",
   },
   {
     title: "Plastics for Change",
     category: "Info about project #3",
     image: require("../Images/portfolio/thumbs/uwblueprint.png"),
-    url: "https://github.com/ahmedhamodi",
+    url: "https://github.com/uwblueprint/plasta",
   },
 ];
 
