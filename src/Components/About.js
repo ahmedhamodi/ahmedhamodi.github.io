651c46a1bddf98d9c79f93d18b7cd82cf6121814
Ahmed Hamodi <ahmed.r.hamodi@gmail.com>
2021-12-28 23:03:40 -0500

    Update resume description fields (#64)

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class About extends Component {
   render() {
     if (this.props.data) {
-      var name = this.props.data.name;
+      var name = this.props.data.firstname + " " + this.props.data.lastname;
       var profilepic = "images/" + this.props.data.image;
       var bio = this.props.data.bio;
       var email = this.props.data.email;
